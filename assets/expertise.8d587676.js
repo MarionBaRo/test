5aca6e497ca0e7519cf78d78c32110164f8ae332
MarionBaRo <105223714+MarionBaRo@users.noreply.github.com>
2022-05-09 16:35:21 +0200

    Add files via upload

---
+++ assets/expertise.8d587676.js
@@ -0,0 +1,1 @@
+import("data:text/javascript;base64,Cg==");import"./main.d22a6ae0.js";import"./vendor.4582d80d.js";
---
+++ assets/managedService1-legacy.0b580af0.js
@@ -0,0 +1,1 @@
+System.register(["./main-legacy.36c0e6a7.js","./vendor-legacy.dd94a3bd.js"],(function(){"use strict";return{setters:[function(){},function(){}],execute:function(){}}}));
