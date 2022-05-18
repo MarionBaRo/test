950a08e2907986a10141ac64385a19691636651f
MarionBaRo <105223714+MarionBaRo@users.noreply.github.com>
2022-05-18 13:39:56 +0200

    Delete assets directory

--- assets/index-legacy.0afbcdfa.js
+++
@@ -1,1 +0,0 @@
-System.register(["./main-legacy.36c0e6a7.js","./vendor-legacy.dd94a3bd.js"],(function(){"use strict";return{setters:[function(){},function(){}],execute:function(){}}}));
--- assets/managedService1.73e342ce.js
+++
@@ -1,1 +0,0 @@
-import("data:text/javascript;base64,Cg==");import"./main.d22a6ae0.js";import"./vendor.4582d80d.js";
