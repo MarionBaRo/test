bd9c33cec88b74358bd68b1b90cc2bcdb732a75d
MarionBaRo <mbastard@altanna.com>
2022-05-19 14:11:35 +0200

    commit

---
+++ partner.js
@@ -0,0 +1,42 @@
+/*
+{
+    cat : ,nom : ,title : ,alt : ,src : }
+*/
+
+var Cybersecurite_Securite = [{
+    cat : "security", nom : "Cybersécurité", title : "Altanna - Cybersécurité", alt : "Altanna - Objet 3d, coffre cadenas illustrant les services de cybersécurité", src : "categories/security.png"}, {
+    cat : "security", nom : "Fortinet", title : "Altanna, partenaire de Fortinet", alt : "Altanna - Logo de Fortinet", src : "partners/fortinet.svg"},{
+    cat : "security", nom : "F5", title : "Altanna, partenaire de F5", alt : "Altanna - Logo de F5", src : "partners/F5Networks.svg"},{
+    cat : "security", nom : "Imperva", title : "Altanna, partenaire de Imperva", alt : "Altanna - Logo de Imperva", src : "partners/Imperva.svg"}];
+
+var Cybersecurite_Reseau = [{
+    cat : "network",nom : "Réseaux",title : "Altanna - Services réseau",alt : "Altanna - Objet 3d illsutrant les services réseau",src : "categories/network.png"},{
+    cat : "network",nom : "Cisco",title : "Altanna, partenaire de Cisco",alt : "Altanna - Logo de Cisco",src : "partners/Cisco.svg"},{
+    cat : "network",nom : "PaloAltoNetworks",title : "Altanna, partenaire de PaloAltoNetworks",alt : "Altanna - Logo de PaloAltoNetworks",src : "partners/PaloAltoNetworks.svg"},{
+    cat : "network",nom : "HewlettPackardEnterprise",title : "Altanna, partenaire de HewlettPackardEnterprise",alt : "Altanna - Logo de HewlettPackardEnterprise",src : "partners/HewlettPackardEnterprise.svg"}];
+
+var Stockage_et_sauvegardex = [{
+    cat : "data-management",nom : "Data management",title : "Altanna - Services de data-management",alt : "Altanna - Objet 3d, coffre-fort illustrant les services de data-management",src : "categories/data-management.png"},{
+    cat : "data-management",nom : "Commvault",title : "Altanna, partenaire de Commvault",alt : "Altanna - Logo de Commvault",src : "partners/Commvault.svg"},{
+    cat : "data-management",nom : "PureStorage",title : "Altanna, partenaire de PureStorage",alt : "Altanna - Logo de PureStorage",src : "partners/PureStorage.svg"},{
+    cat : "data-management",nom : "Netapp",title : "Altanna, partenaire de Netapp",alt : "Altanna - Logo de Netapp",src : "partners/Netapp.svg"},{
+    cat : "data-management",nom : "Veeam",title : "Altanna, partenaire de Veeam",alt : "Altanna - Logo de Veeam",src : "partners/Veeam.svg"},{
+    cat : "data-management",nom : "Ansible",title : "Altanna, partenaire de Ansible",alt : "Altanna - Logo de Ansible",src : "partners/Ansible.svg"}];
+
+var Serveurs = [{
+    cat : "infrastructure-cloud",nom : "Infrastructure DC & Cloud",title : "Altanna - Services d'infrastrutures data-center et cloud",alt : "Altanna - Objet 3d, nuages illustrant les services d'infrastrutures data-center et cloud",src : "categories/infrastructure-cloud.png"},{
+    cat : "infrastructure-cloud",nom : "Windows",title : "Altanna, partenaire de Windows",alt : "Altanna - Logo de Windows",src : "partners/Windows.svg"},{
+    cat : "infrastructure-cloud",nom : "Citrix",title : "Altanna, partenaire de Citrix",alt : "Altanna - Logo de Citrix",src : "partners/Citrix.svg"},{
+    cat : "infrastructure-cloud",nom : "Debian",title : "Altanna, partenaire de Debian",alt : "Altanna - Logo de Debian",src : "partners/Debian.svg"},{
+    cat : "infrastructure-cloud",nom : "Vmware",title : "Altanna, partenaire de Vmware",alt : "Altanna - Logo de Vmware",src : "partners/Vmware.svg"},{
+    cat : "infrastructure-cloud",nom : "Splunk",title : "Altanna, partenaire de Splunk",alt : "Altanna - Logo de Splunk",src : "partners/Splunk.svg"}];
+
+var Supervision = [{
+    cat : "supervision-production",nom : "Supervision & production",title : "Altanna - Services de supervision et production",alt : "Altanna - Objet 3d, support d'écriture avec fiche de soins illustrant les services de supervision et production",src : "categories/supervision-production.png"},{
+    cat : "supervision-production",nom : "Solarwinds",title : "Altanna, partenaire de Solarwinds",alt : "Altanna - Logo de Solarwinds",src : "partners/Solarwinds.svg"},{
+    cat : "supervision-production",nom : "DataDog",title : "Altanna, partenaire de DataDog",alt : "Altanna - Logo de DataDog",src : "partners/DataDog.svg"},{
+    cat : "supervision-production",nom : "Dockers",title : "Altanna, partenaire de Dockers",alt : "Altanna - Logo de Dockers",src : "partners/Dockers.svg"},{
+    cat : "supervision-production",nom : "Kubernetes",title : "Altanna, partenaire de Kubernetes",alt : "Altanna - Logo de Kubernetes",src : "partners/Kubernetes.svg"},{
+    cat : "supervision-production",nom : "Ansible",title : "Altanna, partenaire de Ansible",alt : "Altanna - Logo de Ansible",src : "partners/Ansible.svg"}];
+
+
