1e1150e8c61fe2aa989cb20c27437c071d77cf9a
MarionBaRo <mbastard@altanna.com>
2022-05-19 09:23:39 +0200

    commit

--- assets/lien.js
+++ assets/lien.js
@@ -1,1 +1,3 @@
-import("data:text/javascript;base64,Cg==");import"./main.d22a6ae0.js";import"./vendor.4582d80d.js";
+import("data:text/javascript;base64,Cg==");
+import"./main.d22a6ae0.js";
+import"./vendor.4582d80d.js";
