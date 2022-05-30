ea9a765283b879fb4323e662f182798723fc47bf
MarionBaRo <mbastard@altanna.com>
2022-05-30 14:53:09 +0200

    commit

--- header_footer.js
+++ header_footer.js
@@ -59,19 +59,19 @@ function Header(){
 
                 //Boutons simple
                 for (var i=0; i < Bouton.length; i++){
-                    document.write(`<li><a class="hidden sm:block btn btn-transparent" \
-                        href="`, Bouton[i].href,`">`, Bouton[i].nom,`</a></li> `);
+                    document.write(`<div><a class="hidden sm:block btn btn-transparent" \
+                        href="`, Bouton[i].href,`">`, Bouton[i].nom,`</a></div> `);
                 }       
 
                 // Bouton déroulant
                 for (var i=0; i < Bouton_d.length; i++){
-                    document.write(`<li class = "deroulant"> <a>`, Bouton_d[i].nom,` </a>
+                    document.write(`<div class = "outline-none inline-flex justify-center items-center w-full btn btn-transparent deroulant"> <a>`, Bouton_d[i].nom,` </a>
                         <ul class ="sous">`);
                             for (var j=0; j < Bouton_d[i].liste.length; j++){
-                                document.write(`<li><a href="`,Bouton_d[i].liste[j].href,`">`,Bouton_d[i].liste[j].nom,`</a></li>`);
+                                document.write(`<li><a class="block px-4 py-2 text-sm text-gray-700" id="dropdown-item-`, j,`" href="`,Bouton_d[i].liste[j].href,`">`,Bouton_d[i].liste[j].nom,`</a></li>`);
                             }
                         document.write(`</ul>
-                    </li>`);
+                    </div>`);
                 }
                 
                 document.write(`</nav>`);
