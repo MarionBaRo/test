e3602e1d1c9c7a93f62cb23311e8c75c6194a48e
MarionBaRo <mbastard@altanna.com>
2022-06-21 16:43:14 +0200

    commit

--- assets/header_footer.js
+++ assets/header_footer.js
@@ -198,7 +198,7 @@ function Modal () {
     style="display:none;" \
     x-ref="dialog"> \
         <div class="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0"> \
-            <div onclick=document.getElementById('`, Contact.id,`').style.display='none'
+            <div onclick=document.getElementById('`, Contact.id,`').style.display='block'
                 aria-hidden="true"
                 class="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity"
                 x-description="Background overlay, show/hide based on modal state."
