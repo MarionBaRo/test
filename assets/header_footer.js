3199d29d066a1c1ee306fb7e742990694afdedbb
MarionBaRo <mbastard@altanna.com>
2022-06-22 11:13:35 +0200

    commit

--- assets/header_footer.js
+++ assets/header_footer.js
@@ -197,7 +197,7 @@ function Modal () {
     aria-modal="true" \
     style="display:none;" \
     x-ref="dialog"> \
-        <div class="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0"> \
+        <div class="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0" style="display:block;" > \
             <div onclick=document.getElementById('`, Contact.id,`').style.display='none' \
                 aria-hidden="true" \
                 style="display:block;" \
