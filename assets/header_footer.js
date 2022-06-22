98afd682b5298018b3065a2e604888c985215c1e
MarionBaRo <mbastard@altanna.com>
2022-06-22 10:19:36 +0200

    commit

--- assets/header_footer.js
+++ assets/header_footer.js
@@ -79,13 +79,13 @@ function Header(){
 
                     //Bouton Nous rejoindre - Nous contacter
                     document.write(`<a class="hidden sm:block btn btn-transparent border" \
-                        href="careers.html">Nous rejoindre</a> \
+                        href="`, Rejoindre.href,`">`, Rejoindre.nom,`</a> \
                     <div class="flex-grow" @keydown.window.escape="openContactModal = false"> \
                         <!-- Modal trigger--> \
                         <button onclick="document.getElementById('`, Contact.id,`').style.display='block'" \
                             class="btn btn-primary inline-flex" \
                             type="button"> \
-                            Nous contacter \
+                            `, Contact.nom,` \
                         </button> \
                     </div> \
                 </div> \
@@ -192,13 +192,13 @@ function Footer (){
 };
 
 function Modal () {
-    document.write(`<div id = "`, Contact.id,`" class="fixed z-50 inset-0 overflow-y-auto"
+    document.write(`<div id = "Modal" class="fixed z-50 inset-0 overflow-y-auto"
     aria-labelledby="modal-title" \
     aria-modal="true" \
     style="display:none;" \
     x-ref="dialog"> \
         <div class="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0"> \
-            <div onclick=document.getElementById('`, Contact.id,`').style.display='block'
+            <div onclick=document.getElementById('Modal').style.display='none'
                 aria-hidden="true"
                 class="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity"
                 x-description="Background overlay, show/hide based on modal state."
@@ -212,7 +212,7 @@ function Modal () {
             </div>
             <span aria-hidden="true" \
                 class="hidden sm:inline-block sm:align-middle sm:h-screen"> \
-                                    </span> \
+            </span> \
             <div class="inline-block align-bottom bg-gradient-to-b to-gray-800 from-gray-900 pt-4 text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-xl sm:w-full" \
                 x-description="Modal panel, show/hide based on modal state." \
                 x-show="openContactModal" \
@@ -224,7 +224,7 @@ function Modal () {
                 x-transition:leave-start="opacity-100 translate-y-0 sm:scale-100"> \
                 <div> \
                     <div class="sm:block absolute top-0 right-0 pt-4 pr-4"> \
-                        <button type="button" class="text-gray-50 hover:text-white focus:outline-none" onclick=document.getElementById('`, Contact.id,`').style.display='none'> \
+                        <button type="button" class="text-gray-50 hover:text-white focus:outline-none" onclick=document.getElementById('modal').style.display='none'> \
                             <span class="sr-only">Fermer</span> \
                             <svg class="h-6 w-6" x-description="Heroicon name: outline/x" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true"> \
                                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12"></path> \
