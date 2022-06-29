010e8158c590b49236fd7c8b7c10f1302ef24e18
MarionBaRo <mbastard@altanna.com>
2022-06-29 16:41:29 +0200

    commit

--- assets/header_footer.js
+++ assets/header_footer.js
@@ -34,7 +34,7 @@ var Liste_d = [{
     id:4,name:"Test",url:"./test.html"
   }];
 
-var Liste_d_lien = "#";
+var Liste_d_lien = "./index.html";
 
 function Liste_d_Onclick (i){
     var x = document.getElementById(i);
